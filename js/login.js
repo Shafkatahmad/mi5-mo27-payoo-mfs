563becd04bd3e9c3ecf894cab2f5ae9915d8bfbf
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-28 23:38:00 +0800

    added simple login handler and preventDefault

---
+++ js/login.js
@@ -0,0 +1,10 @@
+// console.log('button clicking file added')
+// search: form submit reloading the page
+document.getElementById('button-login').addEventListener('click', function(event) {
+  // prevent default behavior (prevent reloading browser)
+  event.preventDefault;
+  console.log('console button clicked');
+
+  const phoneNumber = document.getElementById('phone-number').value;
+  console.log(phoneNumber);
+})
